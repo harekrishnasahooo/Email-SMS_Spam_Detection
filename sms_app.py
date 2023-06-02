e995e9732477390166f1678da720235387bed24b
harekrishnasahooo <119866835+harekrishnasahooo@users.noreply.github.com>
2023-06-02 17:23:43 +0530

    Update sms_app.py

--- sms_app.py
+++ sms_app.py
@@ -2,8 +2,9 @@
 import streamlit as st
 import pickle
 import string
-from nltk.corpus import stopwords
 import nltk
+from nltk.corpus import stopwords
+
 from nltk.stem.porter import PorterStemmer
 
 ps = PorterStemmer()
